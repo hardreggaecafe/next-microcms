3a720ff81e576f4b413018d5cd3f8c0c7dca8011
TakamizawaNoriaki <takamizawanoriaki@MacBookAirForNori.local>
2024-05-16 06:14:40 +0900

    fix cache

--- libs/client.js
+++ libs/client.js
@@ -1,7 +1,14 @@
 import { createClient } from 'microcms-js-sdk';
+import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN, 
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
-  retry: true 
+  retry: true,
+  headers: {
+    'Cache-Control': 'no-store',
+    'CDN-Cache-Control': 'no-store',
+    'Vercel-CDN-Cache-Control': 'no-store'
+  }
 });
