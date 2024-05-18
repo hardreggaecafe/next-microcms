48442dce674e58040afd6fe927a8f1575eb7a336
TakamizawaNoriaki <takamizawanoriaki@MacBookAirForNori.local>
2024-05-19 07:42:36 +0900

    Fix cache

--- libs/client.js
+++ libs/client.js
@@ -7,12 +7,9 @@ export const client = createClient({
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
   retry: true,
   customFetch: (input, init) => {
-    if (typeof input === 'string') {
-      const newInput = new URL(input)
-      const time = new Date()
-      newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
-      return fetch(newInput.href, init)
-    }
-    return fetch(input, init)
+    const newInput = new URL(input)
+    const time = new Date()
+    newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
+    return fetch(newInput.href, init)
   },
 });
