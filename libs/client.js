4a4ae298a237117f0dcaea6b1ee1dcec44a8932b
TakamizawaNoriaki <takamizawanoriaki@MacBookAirForNori.local>
2024-05-18 10:32:06 +0900

    Fix cache

--- libs/client.js
+++ libs/client.js
@@ -1,14 +1,17 @@
 import { createClient } from 'microcms-js-sdk';
-import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN, 
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
   retry: true,
-  headers: {
-    'Cache-Control': 'no-store',
-    'CDN-Cache-Control': 'no-store',
-    'Vercel-CDN-Cache-Control': 'no-store'
-  }
+  customFetch: (input, init) => {
+    if (typeof input === 'string') {
+      const newInput = new URL(input)
+      const time = new Date()
+      newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
+      return fetch(newInput.href, init)
+    }
+    return fetch(input, init)
+  },
 });
